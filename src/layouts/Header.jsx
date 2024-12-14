a0fc37c41b4b0a91c46e8aeeaa9a2cc950bac14e
Yasowant <yasowant1998@gmail.com>
2024-12-14 20:13:20 +0530

    prettier dependency install

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
-import { logout } from "../redux/slices/AuthSlice";
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import { logout } from '../redux/slices/AuthSlice';
 
 const Header = () => {
   const navigate = useNavigate();
@@ -10,37 +10,37 @@ const Header = () => {
 
   const handleLogout = () => {
     dispatch(logout());
-    navigate("/login");
+    navigate('/login');
   };
-  console.log(JSON.stringify(localStorage.getItem("Token")));
+  console.log(JSON.stringify(localStorage.getItem('Token')));
 
   return (
     <div className="bg-black h-16 flex justify-between items-center p-2">
       <div className="text-white text-[20px] font-semibold">LMS System</div>
       <ul className="flex gap-2 justify-center items-center list-none">
-       {!isLoggedIn ? (
-        <>
-          <Link to={"/login"}>
-            <li className="bg-white rounded px-2 py-1 text-black font-semibold text-[13px]">
-              Sign in
-            </li>
-          </Link>
-          <Link to={"/register"}>
-            <li className="bg-white rounded px-2 py-1 text-black font-semibold text-[13px]">
-              Sign Up
-            </li>
-          </Link>
-        </>
+        {!isLoggedIn ? (
+          <>
+            <Link to={'/login'}>
+              <li className="bg-white rounded px-2 py-1 text-black font-semibold text-[13px]">
+                Sign in
+              </li>
+            </Link>
+            <Link to={'/register'}>
+              <li className="bg-white rounded px-2 py-1 text-black font-semibold text-[13px]">
+                Sign Up
+              </li>
+            </Link>
+          </>
         ) : (
-        <div className="flex gap-2">
-          <Link to={"/courses"} className="text-white font-semibold">
-            Courses
-          </Link>
-          <i
-            className="bi bi-person-circle text-white"
-            onClick={handleLogout}
-          ></i>
-        </div>
+          <div className="flex gap-2">
+            <Link to={'/courses'} className="text-white font-semibold">
+              Courses
+            </Link>
+            <i
+              className="bi bi-person-circle text-white"
+              onClick={handleLogout}
+            ></i>
+          </div>
         )}
       </ul>
     </div>
--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,20 +1,18 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Header from './Header'
-import Footer from './Footer'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
 
 const Layout = () => {
   return (
-<div>
-<Header/>
+    <div>
+      <Header />
 
-<Outlet/>
+      <Outlet />
 
+      <Footer />
+    </div>
+  );
+};
 
-<Footer/>
-
-</div>
-  )
-}
-
-export default Layout
+export default Layout;
