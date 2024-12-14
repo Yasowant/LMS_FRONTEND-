632e42517e5f8ff9ff044a3b6626d211bf7254a9
Yasowant <yasowant1998@gmail.com>
2024-12-14 21:20:52 +0530

    faq questions and answers is added

---
+++ src/common/constant.js
@@ -0,0 +1,32 @@
+const faqs = [
+  {
+    id: 1,
+    question: "What is an LMS?",
+    answer:
+      "An LMS, or Learning Management System, is a platform designed to manage, deliver, and track educational content and training programs for learners.",
+  },
+  {
+    id: 2,
+    question: "How can an LMS benefit instructors?",
+    answer:
+      "An LMS allows instructors to create, organize, and deliver courses efficiently. It provides tools to track student progress, manage assessments, and communicate effectively with learners.",
+  },
+  {
+    id: 3,
+    question: "Can I access LMS courses on mobile devices?",
+    answer:
+      "Yes, most modern LMS platforms are mobile-friendly, enabling users to access courses anytime, anywhere, on smartphones or tablets.",
+  },
+  {
+    id: 4,
+    question: "Is my progress tracked automatically in an LMS?",
+    answer:
+      "Yes, an LMS tracks your progress automatically, including completed modules, quiz results, and overall performance metrics.",
+  },
+  {
+    id: 5,
+    question: "Can I use an LMS for employee training?",
+    answer:
+      "Absolutely! LMS platforms are commonly used for corporate training to onboard employees, upskill teams, and ensure compliance with industry standards.",
+  },
+];
