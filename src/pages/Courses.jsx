7d0f1ce71523a59687fbe8fc1a5744f814dd5fb7
Yasowant Nayak <129403851+Yasowant@users.noreply.github.com>
2024-12-14 19:56:54 +0530

    Add files via upload
    updated code

---
+++ src/pages/Courses.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllCourses } from "../redux/slices/CourseSlice";
+
+export const Courses = () => {
+const dispatch = useDispatch();
+
+useEffect(()=>{
+  loadAllCourses();
+},[])
+
+
+const loadAllCourses =async  ()=>{
+console.log("Entering coursese");
+  
+  await dispatch(getAllCourses());
+}
+return (
+  <>
+  
+  <h2>Hello World</h2>
+  </>
+)
+};
+
---
+++ src/redux/slices/CourseSlice.js
@@ -0,0 +1,47 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import toast from "react-hot-toast";
+import axiosInstance from "../../Helpers/axiosInstance";
+
+
+const initialState = {
+    courseData:[]
+}
+
+const courseSlice =  createSlice({
+    name:"Courses",
+    initialState,
+    reducers:{},
+    extraReducers:(builder)=>{
+
+    }
+
+
+})
+
+export const getAllCourses = createAsyncThunk('/course/get',async ()=>{
+    try{
+        const response = axiosInstance.get('courses/getallCourses');
+        console.log(response);
+        
+        toast.promise(response,{
+            loading:"loading course data",
+            success:"course loaded successfully",
+            error:"Unable to load course"
+        })
+
+        console.log(response);
+
+        return await response;
+
+   
+        
+    }catch(error){
+        toast.error(error?.response?.data?.message);
+    }
+})
+
+
+export const {} = courseSlice.actions;
+export const courseSliceReducer = courseSlice.reducer;
+export { courseSlice };
+export default courseSlice.reducer;
